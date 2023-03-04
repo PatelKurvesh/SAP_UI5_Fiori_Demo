2a98a33b2961e57e453ed477ac087693c26f341d
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-03-04 19:12:29 +0530

    fragment added

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,13 +10,22 @@ sap.ui.define([
 			var pwd = this.getView().byId("pwd").getValue();
 			
 			if(oName === "admin" && pwd === "admin"){
-				sap.m.MessageToast.show("Success");
+				// sap.m.MessageToast.show("Success");
+				this.frag = new sap.ui.xmlfragment("PracticeSAP_UI5_Fiori_Demo.fragments.success");
+				this.getView().addDependent(this.frag);
+				this.frag.open();
+				
+				
 				
 			}else{
 				sap.m.MessageToast.show("Check your UserName or Password");
 			}
 			
+		},
+		onDialogClose:function(){
+			this.frag.close();
 		}
 
 	});
 });
+
