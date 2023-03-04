1be0c5b0d489274b8f7bf444c72e0c6a4dfa4d51
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-03-04 15:05:36 +0530

    controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,20 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("PracticeSAP_UI5_Fiori_Demo.controller.View1", {
+		
+		onBtnClick : function(){
+			var oName = this.getView().byId("uName").getValue();
+			var pwd = this.getView().byId("pwd").getValue();
+			
+			if(oName === "admin" && pwd === "admin"){
+				sap.m.MessageToast.show("Success");
+				
+			}else{
+				
+				sap.m.MessageToast.show("Check your UserName or Password");
+			}
+			
+		}
 
 	});
 });
